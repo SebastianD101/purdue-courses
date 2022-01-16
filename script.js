3ff8e59df5a091e81be2293d80e1c574046c7917
Sebastian <61755980+SebastianD101@users.noreply.github.com>
2022-01-16 02:31:06 -0500

    fixed mobile support

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function purdueRedirect() {
+    document.getElementById("subject").style.borderColor = "white";
+    document.getElementById("course").style.borderColor = "white";
     var subject = document.getElementById("subject").value;
     var course = document.getElementById("course").value;
     let a = document.createElement('a');
@@ -11,6 +13,8 @@ function purdueRedirect() {
 }
 
 function redditRedirect() {
+    document.getElementById("subject").style.borderColor = "white";
+    document.getElementById("course").style.borderColor = "white";
     var subject = document.getElementById("subject").value;
     var course = document.getElementById("course").value;
     let a = document.createElement('a');
@@ -19,9 +23,12 @@ function redditRedirect() {
         a.href = 'https://www.reddit.com/r/purdue/search?q=' + subject + '+' + course + '&restrict_sr=on&sort=new&t=all';
         a.click();
     }
+    setTimeout(clearDemo, 1000);
 }
 
 function RMCRedirect() {
+    document.getElementById("subject").style.borderColor = "white";
+    document.getElementById("course").style.borderColor = "white";
     var subject = document.getElementById("subject").value;
     var course = document.getElementById("course").value;
     let a= document.createElement('a');
@@ -30,4 +37,11 @@ function RMCRedirect() {
         a.href= 'https://www.ratemycourses.io/purdue/course/' + subject + course;
         a.click();
     }
+    setTimeout(clearDemo, 1000);
+}
+
+function clearDemo()
+{
+    document.getElementById("subject").style.borderColor = '#CEB888';
+    document.getElementById("course").style.borderColor = '#CEB888';
 }
