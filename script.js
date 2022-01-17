722c22c5e2c7e1fff15667f463874c1ce63062dd
Sebastian <61755980+SebastianD101@users.noreply.github.com>
2022-01-16 22:04:35 -0500

    fixed bug with courses

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ function purdueRedirect() {
     document.getElementById("subject").style.borderColor = "white";
     document.getElementById("course").style.borderColor = "white";
     var subject = document.getElementById("subject").value;
+    subject = subject.toUpperCase();
     var course = document.getElementById("course").value;
     let a = document.createElement('a');
+    if (course.length == 3) {
+        course = course.concat("00");
+    }
     if(subject !== "" && course !== "") {
         a.target = '_blank';
         a.href = 'https://selfservice.mypurdue.purdue.edu/prod/bzwsrch.p_catalog_detail?term=202220&subject=' + subject + '&cnbr=' + course;
